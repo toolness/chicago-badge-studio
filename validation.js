6eeb402c0204e49c985c3d0549d46a3de0dee903
Atul Varma <varmaa@gmail.com>
2013-03-25 18:48:54 -0400

    added some documentation to validator.js.

--- validation.js
+++ validation.js
@@ -1,5 +1,23 @@
+// This simple validation library makes it easy to validate untrusted input
+// going into a client-side JavaScript application, e.g. via a querystring.
+// This helps ensure that XSS vulnerabilities are minimized. Note that this
+// has nothing to do with HTML5 Form Validation.
+//
+// Each form element that can be set by an untrusted source is whitelisted
+// via a data-validator attribute. The value of this attribute specifies
+// the name of a validator class, which is responsible for validating
+// untrusted input to set the state of a form element, as well as
+// getting the current state of the form element as a string.
 var Validation = (function() {
   var Validation = {
+    // ## Validation.getState([form])
+    //
+    // Puts the serialized state of all form elements with data-validator
+    // attributes into an object mapping form element ids to their states
+    // and returns it. This object can then be used by the client to e.g.
+    // construct a querystring.
+    // 
+    // If form is not provided, document.body is used.
     getState: function Validation_getState(form) {
       var values = {};
       eachValidator(form, function(validator) {
@@ -7,6 +25,12 @@ var Validation = (function() {
       });
       return values;
     },
+    // ## Validation.setState([form], values)
+    //
+    // Sets the state of form elements, given an object mapping form element
+    // ids to their states.
+    //
+    // If form is not provided, document.body is used.
     setState: function Validation_setState(form, values) {
       if (!values) {
         values = form;
@@ -17,9 +41,16 @@ var Validation = (function() {
           validator.set(values[validator.el.id]);
       });
     },
+    // ## Validation.isSafeUrl(str)
+    //
+    // Returns whether the given string is a "safe" URL, i.e. uses the
+    // http or https protocol.
     isSafeUrl: function Validation_isSafeUrl(str) {
       return /^https?:\/\//.test(str);
     },
+    // ## Validation.types
+    //
+    // This object contains all our validator classes, indexed by name.
     types: {}
   };
 
@@ -38,13 +69,14 @@ var Validation = (function() {
   };
 
   var validator = function(methods) {
-    function Validator(element) {
-      this.el = element;
-    };
+    function Validator(element) { this.el = element; };
     Validator.prototype = methods;
     return Validator;
   };
 
+  // ## boolean validator class
+  //
+  // This validator class should be used for checkboxes.
   Validation.types.boolean = validator({
     get: function() { return this.el.checked.toString(); },
     set: function(value) {
@@ -53,6 +85,10 @@ var Validation = (function() {
     }
   });
 
+  // ## url validator class
+  //
+  // This validator class should be used for text input fields that expect
+  // URL values.
   Validation.types.url = validator({
     get: function() { return this.el.value; },
     set: function(value) {
@@ -60,6 +96,10 @@ var Validation = (function() {
     }
   });
 
+  // ## float validator class
+  //
+  // This validator class should be used for text input fields that expect
+  // float values.
   Validation.types.float = validator({
     get: function() { return this.el.value; },
     set: function(value) {
@@ -67,6 +107,9 @@ var Validation = (function() {
     }
   });
 
+  // ## color validator class
+  //
+  // This validator class should be used for JSColor input fields.
   Validation.types.color = validator({
     get: function() { return this.el.value; },
     set: function(value) {
@@ -74,6 +117,9 @@ var Validation = (function() {
     }
   });
 
+  // ## select validator class
+  //
+  // This validator class should be used for <select> elements.
   Validation.types.select = validator({
     get: function() { return this.el.value; },
     set: function(value) {
